3b1b4ada8e7b09ddd5faf452efa315cd271371a4
WelberthTito <welberthvieiratito@gmail.com>
2022-03-07 09:00:24 -0300

    Estrutura Envio de E-mail

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_mail import Message, Mail
+
+
+app = Flask(__name__)
+
+app.secret_key = ''
+
+
+mail = Mail()
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = ''
+app.config["MAIL_PASSWORD"] = app.secret_key
+
+mail.init_app(app)
+
